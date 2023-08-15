a3e9a098756c2d99df34a2c62556e2155e33956c
Nikita Chernenko <109902609+ChernenkoNS@users.noreply.github.com>
2023-08-15 21:36:56 +0300

    commit-seven

--- script.js
+++ script.js
@@ -1,88 +1,134 @@
 $(document).ready(function () {
   $(".kved-page-wrapper-content-second-step-trigger").click(function () {
-    $(this).next(".kved-page-wrapper-content-second-step-content").toggle();
+    $(this)
+      .next(".kved-page-wrapper-content-second-step-content")
+      .toggleClass("mystyle-invisible");
   });
 });
-
 $(document).ready(function () {
   $(".kved-page-wrapper-content-third-step-trigger").click(function () {
-    $(this).next(".kved-page-wrapper-content-third-step-content").toggle();
+    $(this)
+      .next(".kved-page-wrapper-content-third-step-content")
+      .toggleClass("mystyle-invisible");
   });
 });
 
-$(document).ready(function () {
-  $(".kved-page-wrapper-content-fourth-step-trigger").click(function () {
-    $(this).next(".kved-page-wrapper-content-fourth-step-content").toggle();
-  });
-});
-
-
-
-
-
-const input = document.querySelector('input');
+const input = document.querySelector("input");
 const button = document.querySelector(".header-search-button");
-const closBtn = document.querySelector(".header-search-close-button")
+const cancelBtn = document.querySelector(".header-search-close-button");
 const section = document.querySelectorAll(".kved-container");
+const kvedItem = document.querySelectorAll(
+  ".kved-page-wrapper-content-third-step-content"
+);
 const sectionParent = document.querySelectorAll(
   ".kved-page-wrapper-content-second-step-trigger"
 );
-const qweqwe = document.querySelectorAll(
+const kwedSection = document.querySelectorAll(
   ".kved-page-wrapper-content-second-step-content"
 );
 
+//////////???///////////
+window.onload = function () {
+  for (let i = 0; i < kvedItem.length; i++) {
+    kvedItem[i].classList.add("mystyle-invisible");
+  }
+  for (let i = 0; i < kvedItem.length; i++) {
+    kvedItem[i].children[0].classList.add("kved-page-wrapper-title");
+  }
+};
+///////////////////////
 
-
+////////////////Events///////////////
 button.addEventListener("click", function (event) {
   event.preventDefault();
   const searchValue = input.value;
-  searchParams(searchValue);
+  searchByKvedNumber(searchValue);
 });
 
-closBtn.addEventListener("click", function() {
-  input.value = ''
-  cliarSearchValue() 
+cancelBtn.addEventListener("click", function () {
+  input.value = "";
+  cliarSearchValue();
 });
 
+input.addEventListener("input", (event) => {
+  const searchParams = event.target.value;
+  if (searchParams.length === 0) {
+    cliarSearchValue();
+    
+  }
 
+ 
+});
 
-function searchParams(searchValue) {
+// input.addEventListener("input",
+//   _.debounce(() => {
+//     console.log('qweqw');
+//   }, 3000) )
+
+////////////////////////////////////////////
+
+function searchByKvedText(searchParams) {
+  console.log('qqqqqqq');
   
+  const kvedTitles = document.querySelectorAll(".kved-page-wrapper-title");
+
+  const kvedTitle = [...kvedTitles].filter((title) => {
+    return title.textContent
+      .toLocaleLowerCase()
+      .includes(searchParams.toLocaleLowerCase());
+  });
+  console.log(kvedTitle);
+  
+}
+
+function searchByKvedNumber(searchValue) {
   const search = [...section].find((item) => item.id === searchValue);
 
   if (search) {
-    section.forEach(item => {item.classList.add('mystyle-invisible')})
-    sectionParent.forEach(item => {item.classList.add('mystyle-invisible')})
-
     const childElements = search.children;
     const parentElement = search.parentNode.parentNode;
-    
+
+    section.forEach((item) => {
+      item.classList.add("mystyle-invisible");
+    });
+    sectionParent.forEach((item) => {
+      item.classList.add("mystyle-invisible");
+    });
+
     search.classList.add("mystyle-visible");
-    parentElement.classList.add("mystyle-visible");
-    for (let i = 0; i < childElements.length; i++) {
-      childElements[i].classList.add("mystyle-visible");
-    }
+    parentElement.classList.replace("mystyle-invisible", "mystyle-visible");
+    childElements[1].classList.replace("mystyle-invisible", "mystyle-visible");
+    childElements[0].classList.add("mystyle-invisible");
   } else {
     alert("Данный Квед не найден");
   }
 }
 
-function cliarSearchValue()  {
-  console.log('Cliar');
-  
-  section.forEach(item => {item.classList.remove('mystyle-invisible', 'mystyle-visible')})
-  sectionParent.forEach(item => {item.classList.remove('mystyle-invisible')})
-  qweqwe.forEach(item => {item.classList.remove('mystyle-visible')})
+function cliarSearchValue() {
+  console.log("Cliar");
 
-}
-
-input.addEventListener('input', event => {
-  console.log(event.target.value);
-  if(event.target.value.length === 0) {
-    cliarSearchValue() 
+  for (let i = 0; i < section.length; i++) {
+    const item = section[i].children;
+    for (let i = 0; i < item.length; i++) {
+      item[i].classList.replace("mystyle-visible", "mystyle-invisible");
+    }
   }
-  
-})
+  //////////???/////////
+  for (let i = 0; i < section.length; i++) {
+    const item = section[i].children;
+    for (let i = 0; i < item.length; i++) {
+      item[0].classList.remove("mystyle-invisible");
+    }
+  }
+  //////////////////////////
 
-
-
+  section.forEach((item) => {
+    item.classList.remove("mystyle-invisible", "mystyle-visible");
+  });
+  sectionParent.forEach((item) => {
+    item.classList.remove("mystyle-invisible", "mystyle-visible");
+  });
+  kwedSection.forEach((item) => {
+    item.classList.replace("mystyle-visible", "mystyle-invisible");
+  });
+}
